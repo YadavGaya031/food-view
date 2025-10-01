ee158cded6ed79ee6d91011ab30143675dfac8f7
Gaya <143161293+YadavGaya031@users.noreply.github.com>
2025-10-02 03:54:28 +0530

    Update api.js

--- frontend-v2/src/lib/api.js
+++ frontend-v2/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:3000', withCredentials: true })
+const api = axios.create({ baseURL: 'https://food-view-fvig.onrender.com', withCredentials: true })
 
 export default api
 
